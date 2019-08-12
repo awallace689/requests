12ca5fc99faf733aaf1a2e4871ab2c93d8fc0249
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2019-08-12 01:27:22 -0500

    refactored main.py

--- src/main.py
+++ src/main.py
@@ -5,19 +5,22 @@ from lib.my_requests import ResponseWrapper, get_json
 URL = 'https://api.github.com/repos/awallace689/requests/events'
 
 
-def formatted_print(to_print, label: str) -> None:
-    print(
-        f'################ {label}',
-        to_print,
-        f'################ END {label}',
-        sep='\n')
+def debug_response(resp: ResponseWrapper):
+    def formatted_print(to_print, label: str) -> None:
+        print(
+            f'################ BEGIN {label}',
+            to_print,
+            f'################ END {label}',
+            sep='\n')
+
+    formatted_print(j.dumps(resp.source, indent=2), 'source')
+    formatted_print(j.dumps(resp.models, indent=2), 'models')
+    print(f'[source==models: {j.dumps(resp.models == resp.source)}]')
 
 
 def _main() -> None:
     resp = ResponseWrapper(get_json(URL))
-    formatted_print(j.dumps(resp.source, indent=2), 'source')
-    formatted_print(j.dumps(resp.models, indent=2), 'models')
-    print(j.dumps(resp.models == resp.source))
+
 
 if __name__ == "__main__":
     _main()
