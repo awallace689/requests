39dc586de41e0f7d46f3548694a91bfb32666665
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2019-08-12 01:17:32 -0500

    Created my_requests.py library

---
+++ src/lib/my_requests.py
@@ -0,0 +1,52 @@
+from typing import List, Set
+import json as j
+from copy import deepcopy
+from typing import Dict, List
+from requests import get as _get, models
+
+
+class _run:
+  """Request count, limit"""
+  call_count = 0
+  call_limit = 10
+
+
+class ResponseWrapper:
+  """'requests' response wrapper for api.github.com/repos/<user>/<repo>/events"""
+
+  def __init__(self, json):
+    self._source: List[dict] = json
+    self._models: Set[dict] = self._get_models()
+    
+  @property
+  def models(self) -> str:
+    return self._models
+
+  @property
+  def source(self) -> List[dict]:
+    return deepcopy(self._source)
+  
+  def _get_models(self):
+    models = []
+    added = []
+    for obj in self._source:
+      if not obj.keys() in added:
+        added.append(obj.keys())
+        models.append(obj)
+    return models
+  
+
+def get(url: str) -> models.Request:
+  """Restrict # of calls to _run.call_limit"""
+  if _run.call_count < _run.call_limit:
+    _run.call_count += 1
+    return _get(url)
+  else:
+    raise RuntimeError(
+        f'ERROR: Call limit ({str(_run.call_count)}) exceeded.')
+
+
+def get_json(url: str) -> dict:
+  """Get Request as dict (json)"""
+  resp = get(url)
+  return resp.json()
--- src/main.py
+++ src/main.py
@@ -1,49 +1,23 @@
-import requests
 import json as j
+from lib.my_requests import ResponseWrapper, get_json
 
 
 URL = 'https://api.github.com/repos/awallace689/requests/events'
 
 
-# get request counter / lifespan
-class run:
-    call_count = 0
-    call_limit = 10
-
-
-class GitHub_Response:
-    """'requests' response wrapper for api.github.com/repos/<user>/<repo>/events"""
-
-    def __init__(self, json):
-        self.source = json
-        self._model = j.dumps(json[0], indent=2)
-
-    @property
-    def model(self) -> dict:
-        return self._model
-
-
-def get(url: str) -> requests.models.Request:
-    """Restrict # of calls to run.call_limit"""
-    if run.call_count < run.call_limit:
-        print(run.call_count)
-        run.call_count += 1
-        return requests.get(url)
-    else:
-        raise RuntimeError(
-            f'ERROR: Call limit {str(run.call_count)} exceeded.')
-
-
-def get_json(url: str) -> dict:
-    """Get Request as dict (json)"""
-    resp = get(url)
-    return resp.json()
+def formatted_print(to_print, label: str) -> None:
+    print(
+        f'################ {label}',
+        to_print,
+        f'################ END {label}',
+        sep='\n')
 
 
 def _main() -> None:
-    resp = GitHub_Response(get_json(URL))
-    print(resp.model)
-
+    resp = ResponseWrapper(get_json(URL))
+    formatted_print(j.dumps(resp.source, indent=2), 'source')
+    formatted_print(j.dumps(resp.models, indent=2), 'models')
+    print(j.dumps(resp.models == resp.source))
 
 if __name__ == "__main__":
     _main()
