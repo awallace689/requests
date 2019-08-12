c609f55f3ec4359ced1938a0ca71e97ab810602c
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2019-08-11 22:59:41 -0500

    Refined utility functions

--- src/main.py
+++ src/main.py
@@ -2,73 +2,70 @@ import requests
 import json as j
 
 
+URL = 'https://api.github.com/repos/awallace689/requests/events'
+
+
 # get request counter / lifespan
 class run:
     call_count = 0
+    call_limit = 10
 
 
 class GitHub_Response:
+    """'requests' response wrapper for api.github.com/repos/<user>/<repo>/events"""
     def __init__(self, json):
         self.source = json
-        self._model = json
+        self._model = j.dumps(json[0], indent=2)
 
     @property
-    def model(json: dict):
+    def model(self) -> dict:
         return self._model
-
-    @model.setter
-    def model(json):
+            
 
 
 def get(url: str) -> requests.models.Request:
-    if r.call_count < 10:
-        requests.get(url)
+    """Restrict # of calls to run.call_limit"""
+    if run.call_count < run.call_limit:
+        print(run.call_count)
+        run.call_count += 1
+        return requests.get(url)
+    else:
+        raise RuntimeError(f'ERROR: Call limit {str(run.call_count)} exceeded.')
 
 
 def get_json(url: str) -> dict:
-    resp = requests.get(url)
+    """Get Request as dict (json)"""
+    resp = get(url)
     return resp.json()
 
 
-def pretty_str(resp: dict) -> dict:
-    """Dict -> String, indent=2"""
-    return j.dumps(resp, indent=2)
-
-
-def get_and_print(url: str) -> None:
-    """Pretty print 'get' response from url."""
-    r = pretty_str(get_json(url))
-    print(r)
-    return r
-
-
 def _main() -> None:
-    r = get_and_print(
-        'https://api.github.com/repos/awallace689/requests/events')
+    resp = GitHub_Response(get_json(URL))
+    print(resp.model)
 
-
+    
 if __name__ == "__main__":
     _main()
 
 
-class C(object):
+# class C(object):
 
 
-def __init__(self):
-    self._x = None
+# def __init__(self):
+#     self._x = None
 
 
-@property
-def x(self):
-    """I'm the 'x' property."""
-    return self._x
+# @property
+# def x(self):
+#     """I'm the 'x' property."""
+#     return self._x
 
 
-@x.setter
-def x(self, value):
-    self._x = value
+# @x.setter
+# def x(self, value):
+#     self._x = value
 
 
-@x.deleter
-def x(self):
-    del self._x
+# @x.deleter
+# def x(self):
+#     del self._x
