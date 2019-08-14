a3d437edf231872acd1be1a99eb6ccb6cf4787fb
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2019-08-13 19:02:14 -0500

    Template displays URL

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flaskr.lib.myrequests.getlib import get_json, ResponseWrapper
+from asyncio import run
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ app = Flask(__name__)
 @app.route('/home/')
 @app.route('/home/<url>')
 def home(url='https://api.github.com/repos/awallace689/requests/events'):
-  return render_template('home.html', URL=url)
+  resp = ResponseWrapper(get_json(url))
+  return render_template(
+    'home.html', URL=url, source=resp.source, models=resp.models)
